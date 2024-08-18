167e67caeeee5d218e1c3c61967960d293e26616
Mao De Matos <mao.dematos@gmail.com>
2024-08-18 02:29:34 +0200

    🚧 backend: Add basic ws server

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func reader(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		fmt.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+	// log statement to show connections
+	log.Println("Client Connected")
+
+	err = ws.WriteMessage(1, []byte(
+		fmt.Sprintf(`{"text": "Hello WS !", "timestamp": %d}`, time.Now().UnixMilli()),
+	))
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	reader(ws)
+}
+
+func setupRoutes() {
+	http.HandleFunc("/ws", wsEndpoint)
+}
+
+func main() {
+	setupRoutes()
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
